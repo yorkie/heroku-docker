ff6fb95224268c7021b19a505ce0c6b55bf8da17
Yorkie Liu <yorkiefixer@gmail.com>
2015-05-19 10:45:06 +0800

    remove unsquashfs-tools

--- init.rb
+++ init.rb
@@ -177,11 +177,9 @@ private
     envs = env.keys.sort.map { |key| "ENV #{key} #{env[key]}" }.join("\n")
     IO.write("#{dir}/Dockerfile", <<-DOCKERFILE.split("\n").map { |l| l.strip }.join("\n"))
       FROM #{base}
-      RUN apt-get update
-      RUN apt-get install squashfs-tools
       RUN rm -rf /app
       RUN curl '#{url}' -o /slug.img
-      RUN (unsquashfs -d /app /slug.img || (cd / && mkdir /app && tar -xzf /slug.img)) && rm -f /app/log /app/tmp && mkdir /app/log /app/tmp &&  chown -R daemon:daemon /app && chmod -R go+r /app && find /app -type d | xargs chmod go+x
+      RUN (cd / && mkdir /app && tar -xzf /slug.img) && rm -f /app/log /app/tmp && mkdir /app/log /app/tmp &&  chown -R daemon:daemon /app && chmod -R go+r /app && find /app -type d | xargs chmod go+x
       ADD database.yml /app/config/database.yml
       #{envs}
       WORKDIR /app
